796859641b9bba5855709d7a9fad7fde0642bf5a
Piotr Godzień <piotrgodzien@gmail.com>
2023-07-07 14:44:53 +0200

    Update page.tsx

--- app/strony-internetowe/page.tsx
+++ app/strony-internetowe/page.tsx
@@ -36,10 +36,10 @@ export default function Strony() {
       <meta name="title" content="Strony internetowe – Getpromo agencja marketingowa" />
       <meta name="description" content="Tworzenie stron internetowych, projektowanie stron www, sklepy internetowe, landing page." />
       <link
-          rel="canonical"
-          href="https://getpromo.pl/strony-internetowe"
-          key="canonical"
-        />
+        rel="canonical"
+        href="https://getpromo.pl/strony-internetowe"
+        key="canonical"
+      />
 
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://getpromo.pl/" />
@@ -62,33 +62,33 @@ export default function Strony() {
                 Strony <br /> internetowe</h1>
               <p className="max-w-3xl relative md:text-xl z-10 text-white ">Tworzymy skuteczne strony internetowe, które pracuja i zarabiają dla nszych klientów 24 godziny na dobę przez 7 dni w tygodniu.</p>
               <button className="cursor-pointer" type="button">
-                                <a href="#kontakt" className="flex items-center">
-                                    <div className="hidden md:block w-24 relative z-50 h-[1px] bg-primary"></div>
-                                    <div className="relative overflow-clip border border-primary z-10 group flex">
-                                        <span className="absolute w-full h-full -left-full bg-transparent z-10 group-hover:left-0 duration-500 box-border group-hover:transition-all group-hover:duration-500 group-hover:bg-primary"></span>
-                                        <div className="relative group-hover:bg-transparent bg-primary h-16">
-                                            <p className="flex relative z-20 items-center justify-center h-16 px-8 font-serif font-medium text-center group-hover:text-black">
-                                                skontaktuj się</p>
-                                        </div>
-                                        <div className="flex items-center justify-center w-16 h-16">
-                                            <svg
-                                                className="fill-white relative z-20 group-hover:fill-black duration-500 group-hover:transition-all"
-                                                width="24"
-                                                height="19"
-                                                viewBox="0 0 24 19"
-                                                fill="none"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                            >
-                                                <path
-                                                    fillRule="evenodd"
-                                                    clipRule="evenodd"
-                                                    d="M23.853 9.85425L14.854 18.8542C14.756 18.9512 14.628 19.0002 14.5 19.0002C14.373 19.0002 14.245 18.9512 14.147 18.8542C13.952 18.6582 13.952 18.3422 14.147 18.1462L22.293 10.0002H0.5C0.224 10.0002 0 9.77625 0 9.50025C0 9.22425 0.224 9.00025 0.5 9.00025H22.293L14.147 0.85425C13.952 0.65825 13.952 0.34225 14.147 0.14625C14.342 -0.04875 14.659 -0.04875 14.854 0.14625L23.853 9.14625C23.9 9.19225 23.937 9.24825 23.962 9.30925C24.012 9.43125 24.012 9.56925 23.962 9.69125C23.937 9.75225 23.9 9.80825 23.853 9.85425"
-                                                />
-                                            </svg>
-                                        </div>
-                                    </div>
-                                </a>
-                            </button>
+                <a href="#kontakt" className="flex items-center">
+                  <div className="hidden md:block w-24 relative z-50 h-[1px] bg-primary"></div>
+                  <div className="relative overflow-clip border border-primary z-10 group flex">
+                    <span className="absolute w-full h-full -left-full bg-transparent z-10 group-hover:left-0 duration-500 box-border group-hover:transition-all group-hover:duration-500 group-hover:bg-primary"></span>
+                    <div className="relative group-hover:bg-transparent bg-primary h-16">
+                      <p className="flex relative z-20 items-center justify-center h-16 px-8 font-serif font-medium text-center group-hover:text-black">
+                        skontaktuj się</p>
+                    </div>
+                    <div className="flex items-center justify-center w-16 h-16">
+                      <svg
+                        className="fill-white relative z-20 group-hover:fill-black duration-500 group-hover:transition-all"
+                        width="24"
+                        height="19"
+                        viewBox="0 0 24 19"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M23.853 9.85425L14.854 18.8542C14.756 18.9512 14.628 19.0002 14.5 19.0002C14.373 19.0002 14.245 18.9512 14.147 18.8542C13.952 18.6582 13.952 18.3422 14.147 18.1462L22.293 10.0002H0.5C0.224 10.0002 0 9.77625 0 9.50025C0 9.22425 0.224 9.00025 0.5 9.00025H22.293L14.147 0.85425C13.952 0.65825 13.952 0.34225 14.147 0.14625C14.342 -0.04875 14.659 -0.04875 14.854 0.14625L23.853 9.14625C23.9 9.19225 23.937 9.24825 23.962 9.30925C24.012 9.43125 24.012 9.56925 23.962 9.69125C23.937 9.75225 23.9 9.80825 23.853 9.85425"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                </a>
+              </button>
             </div>
             <div className="w-full absolute inset-0 lg:relative overflow-hidden">
               <Image className="relative" alt="design-god" src={goddess} />
@@ -99,7 +99,7 @@ export default function Strony() {
               </div>
             </div>
           </div>
-          <Image className="hidden -z-10 lg:block mx-auto absolute top-96 inset-0" src={pattern} alt="" />
+          <Image className="hidden -z-30 lg:block mx-auto absolute top-96 inset-0" src={pattern} alt="" />
         </section>
         <section className="text-center relative z-10 my-12">
           <div className="flex flex-wrap md:flex-row flex-col max-w-7xl px-8 md:space-y-0 space-y-12 mx-auto justify-center">
@@ -254,7 +254,7 @@ export default function Strony() {
           <Image className="xl:border-4 border-2 border-white shadow-lg" alt="Strona dla producenta przetworów owocowych" src={agrofruct} />
         </div>
       </section>
-<CtaFooter />
+      <CtaFooter />
     </>
   );
 }
