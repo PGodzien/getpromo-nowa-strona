5fdea11b8ee25a103f4fdce1009095e01a999d86
Piotr Godzień <piotrgodzien@gmail.com>
2023-07-07 16:06:26 +0200

    Update testimonials.tsx

--- app/components/testimonials.tsx
+++ app/components/testimonials.tsx
@@ -10,8 +10,8 @@ export const Testimonials = () => {
           <div className="font-serif mx-auto">
             <div className="space-y-3 px-4 lg:px-0 pb-8 md:pb-16">
               <p className="font-serif leading-tight text-3xl md:text-4xl font-semibold mb-10 lg:text-5xl">Klienci o nas</p>
-              <p className="md:text-xl font-sans">Naszym głównym celem jest zapewnienie sukcesu naszym klientom, ponieważ wiemy, że zadowoleni klienci przyciągają nowych.</p>
-              <p className="md:text-xl font-sans hidden md:block">Dokładamy starań, aby nasza współpraca z klientem opierała się na przyjaznym partnerstwie, pełnej transparentności i wyborze rozwiązań dopasowanych do oczekiwanych efektów biznesowych.</p>
+              <p className="md:text-xl font-sans text-gray-300">Naszym głównym celem jest zapewnienie sukcesu naszym klientom, ponieważ wiemy, że zadowoleni klienci przyciągają nowych.</p>
+              <p className="md:text-xl font-sans text-gray-300 hidden md:block">Dokładamy starań, aby nasza współpraca z klientem opierała się na przyjaznym partnerstwie, pełnej transparentności i wyborze rozwiązań dopasowanych do oczekiwanych efektów biznesowych.</p>
             </div>
             <Opinions />
           </div>
