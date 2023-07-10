60f17b2cef37018aeb147573088680a27379c011
Piotr Godzień <piotrgodzien@gmail.com>
2023-07-10 22:03:55 +0200

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,9 @@ import './globals.css';
 import localFont from '@next/font/local';
 import NavBar from './components/nav';
 import Footer from './components/footer';
-import Analytics from './analytics';
 import { Suspense } from 'react';
 import Script from 'next/script';
-
-const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
+const GTM_ID = 'GTM-GTM-KGZT6KS';
 
 const clashGrotesk = localFont({
   src: [
@@ -201,22 +199,25 @@ export default function RootLayout({
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
           strategy="afterInteractive"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-tag-manager" strategy="afterInteractive">
           {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){window.dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', '${GA_MEASUREMENT_ID}');
-  `}
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
         </Script>
       </head>
       <body className="bg-getDark">
-        <Suspense>
-          <Analytics />
-        </Suspense>
         <NavBar />
         {children}
         <Footer />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
       </body>
     </html>
   )
