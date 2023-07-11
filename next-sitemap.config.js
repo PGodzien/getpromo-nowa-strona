0801625dce1d763ac337a3db3a9e7122573939e0
Piotr Godzień <piotrgodzien@gmail.com>
2023-07-11 10:51:41 +0200

    .

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://getpromo.pl',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
