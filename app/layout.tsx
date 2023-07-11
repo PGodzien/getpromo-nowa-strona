eb2aa39782c3c58552a415778107a8d51c07e0ab
Piotr Godzień <piotrgodzien@gmail.com>
2023-07-11 06:35:29 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -208,11 +208,11 @@ export default function RootLayout({
         <NavBar />
         {children}
         <Footer />
-        <noscript
+        {/* <noscript
           dangerouslySetInnerHTML={{
             __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
           }}
-        />
+        /> */}
       </body>
     </html>
   )
