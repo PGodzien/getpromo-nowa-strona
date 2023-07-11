f8cb0f55dd31c68fb50d399128ddf764c1b055b4
Piotr Godzień <piotrgodzien@gmail.com>
2023-07-11 11:11:49 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Clients from './components/clients';
 import CtaFooter from './components/ctaFooter';
 import Features from './components/features';
 import Footer from './components/footer';
+import { Analytics } from '@vercel/analytics/react';
 import Hero from './components/hero';
 import { Testimonials } from './components/testimonials';
 
 export default function Home() {
   return (
     <>
+    <Analytics />
       <Hero />
       <Features />
       <Clients />
