4a73b2bca063cbf8431e7498b295c58f573dbd02
Piotr Godzień <piotrgodzien@gmail.com>
2023-10-20 12:21:10 +0200

    Update page.tsx

--- app/strony-internetowe-rzeszow/page.tsx
+++ app/strony-internetowe-rzeszow/page.tsx
@@ -121,7 +121,7 @@ export default function StronyRzeszow() {
             </div>
             <div className="relative lg:w-1/2">
               <Image className="relative" alt="design-god" src={webRzeszow} />
-              <div className="animate-flash -z-10 w-10 h-10 p-64 xl:p-80 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-3">
+              <div className="animate-flash -z-10 w-10 h-10 p-44 sd:p-64 xl:p-80 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-3">
                 <div className="absolute inset-0 z-10 m-0.5  bg-transparent border-8 border-blue-500 blur rounded-full"></div>
                 <div className="absolute inset-0 z-20  bg-transparent border-4 border-white rounded-full"></div>
                 <div className="absolute inset-0 z-10 -m-1  bg-transparent border-8 border-blue-500 blur rounded-full"></div>
