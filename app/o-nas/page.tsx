33257cd4b845f88207620ee8b1b7a5396101ca13
Piotr Godzień <piotrgodzien@gmail.com>
2023-10-22 20:53:08 +0200

    Update page.tsx

--- app/o-nas/page.tsx
+++ app/o-nas/page.tsx
@@ -163,7 +163,7 @@ export default function Strony() {
             <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
               <h1 className="mt-2 text-3xl font-medium font-serif tracking-tight text-gray-900 sm:text-4xl">O nas</h1>
               <p className="mt-6 text-xl leading-8">
-                Otherland jest agencją kreatywną, która pomaga firmom i startupom w marketingu ich produktów. Używamy najnowocześniejszych technologii, aby pomóc naszym klientom zrozumieć konsumentów, przekształcić ich w klientów i rozwijać ich marki.
+                Getpromo jest agencją kreatywną, która pomaga firmom i startupom w marketingu ich produktów. Używamy najnowocześniejszych technologii, aby pomóc naszym klientom zrozumieć konsumentów, przekształcić ich w klientów i rozwijać ich marki.
               </p>
               <div className="mt-10 max-w-2xl">
                 <p>
