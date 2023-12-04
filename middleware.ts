66479b38dc23f2132f271947ef36e4bf150cefb9
Piotr Godzień <piotrgodzien@gmail.com>
2023-12-04 16:06:02 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,24 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+interface Redirects {
+  [key: string]: string;
+}
+
+const redirects: Redirects = {
+  "/contact/": "/kontakt/",
+  "/realization/": "/",
+  "/kampanie-internetowe//": "/marketing/",
+  "/marketing-internetowy/": "/marketing/",
+  "/social-media/": "/marketing/",
+};
+
 export function middleware(request: NextRequest) {
   const url = request.nextUrl.clone();
-  if (url.pathname === "/contact/") {
-    url.pathname = "/kontakt/";
-    return NextResponse.redirect(url);
-  }
-  if (url.pathname === "/realization/") {
-    url.pathname = "/";
-    return NextResponse.redirect(url);
-  }
-  if (url.pathname === "/kampanie-internetowe//") {
-    url.pathname = "/marketing/";
-    return NextResponse.redirect(url);
-  }
-  if (url.pathname === "/marketing-internetowy/") {
-    url.pathname = "/marketing/";
-    return NextResponse.redirect(url);
-  }
-  if (url.pathname === "/social-media/") {
-    url.pathname = "/marketing/";
+  const redirectTo = redirects[url.pathname];
+
+  if (redirectTo) {
+    url.pathname = redirectTo;
     return NextResponse.redirect(url);
   }
 }
